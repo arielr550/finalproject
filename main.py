057795832edb2c0f37cd1263957c6c6a874c01d0
Ariel <arielr550@gmail.com>
2025-10-03 16:04:50 +0300

    need to handle sort salaries

--- main.py
+++ main.py
@@ -55,6 +55,9 @@ class Flight:
         self.price = price
         self.people_queue = Queue()
         self.suitcase_stack = Stack()
+        # New lists for suitcase choices
+        self.no_suitcase_by_choice = []
+        self.no_suitcase_overweight = []
 
     def __repr__(self):
         return (f"Flight ID: {self.flight_id}, Destination: {self.dest}, Day: {self.day}, Month: {self.month}, "
@@ -68,7 +71,7 @@ class Airport:
         self.employee_list = []
 
     def add_flight(self):
-        print('***Flight Addition***')
+        print('*** Flight Addition ***')
         flight_id = int(input('Enter Flight ID: '))
         dest = input('Enter Destination: ')
         day = int(input('Enter day date (number): '))
@@ -80,7 +83,7 @@ class Airport:
         self.flight_list.append(flight)
 
     def add_customer(self):
-        print('***Customer Addition***')
+        print('*** Customer Addition ***')
         p_id = int(input('Enter your ID: '))
         name = input('Enter your name: ')
         age = int(input('Enter your age: '))
@@ -120,13 +123,15 @@ class Airport:
                 break
         if chosen_flight is None:
             return "Invalid flight choice!"
-        chosen_flight.people_queue.enqueue(customer)
-        chosen_flight.price = chosen_flight.price * 1.02
+
 
         suitcase_choice = input('Are you flying with a suitcase? ("yes" or "no"): ')
         if suitcase_choice.lower() == 'yes':
             weight = float(input('What is the weight of the suitcase? '))
             suitcase = Suitcase(customer, weight)
+
+            chosen_flight.people_queue.enqueue(customer)
+            chosen_flight.price = chosen_flight.price * 1.02
 
             if customer.vip:
                 #If customer is VIP
@@ -163,9 +168,17 @@ class Airport:
 
                 if current_weight + weight > max_weight:
                     # Check if weight exceeded
+                    chosen_flight.no_suitcase_overweight.append(customer)
                     return "Suitcase cannot enter - weight limit exceeded!"
                 # Add non-vip suitcase at the top of the stack
                 chosen_flight.suitcase_stack.push(suitcase)
+
+        elif suitcase_choice.lower() == 'no':
+            chosen_flight.no_suitcase_by_choice.append(customer)
+            chosen_flight.people_queue.enqueue(customer)
+            chosen_flight.price = chosen_flight.price * 1.02
+
+
         return f"{customer.name} was added to flight {chosen_flight.flight_id} to {chosen_flight.dest} successfully!"
 
 
@@ -187,7 +200,7 @@ class Airport:
                 break
         if employee is None:
             return 'No such employee!'
-        print('***Work Day Addition***')
+        print('*** Work Day Addition ***')
         day = int(input('Enter day: '))
         month = int(input('Enter month: '))
         work_hours = int(input('Enter amound of work hours: '))
@@ -210,8 +223,30 @@ class Airport:
             current = current.next
         monthly_salary = total_hours * employee.hour_sal
         return f"Employee {employee.name}'s salary for {month} is: {monthly_salary}"
-
-
+    
+    def flight_ended(self, flight_id):
+        landing_flight = None
+        for flight in self.flight_list:
+            if flight.flight_id == flight_id:
+                landing_flight = flight
+                print(f'Flight {flight_id} is landing!')
+                break
+            else:
+                return f'Flight {flight_id} do not exist!'
+        if landing_flight is None:
+            return f'Flight {flight_id} do not exist'
+        
+        print('*** Suitcases Incoming ***')
+        while not landing_flight.suitcase_stack.is_empty():
+            print(landing_flight.suitcase_stack.pop())
+        if landing_flight.no_suitcase_by_choice:
+            print('Names of people without suitcase by choice: ')
+            for customer in landing_flight.no_suitcase_by_choice:
+                print(customer.name)
+        if landing_flight.no_suitcase_overweight:
+            print('Names of people without suitcase because overweight: ')
+            for customer in landing_flight.no_suitcase_overweight:
+                print(customer.name)        
 
 
     def __repr__(self):
@@ -220,10 +255,58 @@ class Airport:
 
 
 
-person = Person(1, 'Ariel', 26)
 airport = Airport('TLV')
-airport.add_flight()
-airport.add_flight()
-airport.add_customer()
-print(airport)
-print(airport.choose_flight())
+# airport.add_flight()
+# airport.add_flight()
+# airport.add_customer()
+# airport.add_customer()
+# print(airport)
+# print(airport.choose_flight())
+# airport.flight_ended(325)
+
+
+
+def menu():
+    print(f"Welcome to {airport.airport_code} Airport, please select an option: ")
+    print('===============================')
+    print('1. Add a flight')
+    print('2. Register new customer')
+    print('3. Get flight recommendation')
+    print('4. View landing flight summary')
+    print('5. Register new employee')
+    print('6. Update employee work hours')
+    print('7. View sorted employee salaries')
+    print('8. Display employee salary by ID')
+    print('Type "quit" to exit')
+    print('===============================')
+
+    while True:
+        choice = input('\nEnter your choice (1-8 or "quit"): ')
+        if choice.lower() == 'quit':
+            print('Goodbye!')
+            break
+        elif choice == '1':
+            airport.add_flight()
+        elif choice == '2':
+            airport.add_customer()
+        elif choice == '3':
+            airport.choose_flight()
+        elif choice == '4':
+            f_id = int(input('Enter flight ID: '))
+            airport.flight_ended(f_id)
+        elif choice == '5':
+            airport.add_employee()
+        elif choice == '6':
+            airport.add_workday()
+        elif choice == '7':
+            # sort salaries
+            pass
+
+
+            
+        elif choice == '8':
+            e_id = int(input('Enter employee ID: '))
+            airport.employee_salary(e_id)
+
+
+print(menu())
